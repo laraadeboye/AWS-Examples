2d670c279084d3f6badb5ed72c3f2939542354cc
Omolara Adeboye <126986992+laraadeboye@users.noreply.github.com.>
2024-06-15 09:15:50 +0000

    Update gitpod.yml with pwsh install modules

--- s3/powershell-scripts/s3.ps1
+++ s3/powershell-scripts/s3.ps1
@@ -7,6 +7,6 @@ $bucketName = Read-Host Prompt "Enter the S3 bucket name"
 Write-Host "AWS Region: $region"
 Write-Host "S3 Bucket: $bucket Name"
 
-New-S3Bucket BucketName $bucketName Region $region
+New-S3Bucket -BucketName $bucketName -Region $region
 
 # Create a new file
